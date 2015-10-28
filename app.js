e262b2de2a8e3fe3b1bde352e2324b7d85c26e8d
Ke Wang <wangkekekexili@gmail.com>
2015-10-28 10:59:06 -0400

    Add user API.

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var app = express();
 app.use(bodyParser.json());
 
 // Include the Todo-lists service and let it bind to the application
+var userService = require('./services/user.js');
+userService.LinkApp(app);
 var todoService = require('./services/todo.js');
 todoService.LinkApp(app);
 
@@ -12,5 +14,5 @@ var server = app.listen(8080, function() {
 	var host = server.address().address;
 	var port = server.address().port;
 
-	console.log('Direct your browser to http://%s:%s/api/todo-lists', host, port);
+	console.log('Direct your browser to http://%s:%s/api/users', host, port);
 });
